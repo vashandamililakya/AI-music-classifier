a4160b9ca52e8ae11fb7a704925fd32a166dcdd0
vashandamililakya <vashandamililakya07@gmail.com>
2026-04-27 23:36:38 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Install ffmpeg for pydub (MP3 / M4A / AAC / WebM decoding)
-apt-get update -y && apt-get install -y ffmpeg
-
-# Install Python dependencies
-pip install -r requirements.txt
